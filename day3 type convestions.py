862e78efa0ffcb94b9cc0f00315acb1af263f0eb
koviri satya sai shiva <satyasaishivakoviri2004@gmail.com>
2026-07-20 12:40:39 +0530

    Add files via upload
    learned about type conversions

---
+++ day3 type convestions.py
@@ -0,0 +1,75 @@
+'''
+TYPE COVERSIONS
+
+___integer:- int()
+
+example:
+
+how= "i have 67"
+print(type(how))
+who = int(how)
+print(type(who))____________ #not possible
+
+num = 9
+print(type(num))
+
+___string:- str()
+
+example:
+
+num = 9
+print(type(num))
+so = str(num)
+print(so)
+
+___float:- float()
+
+example:
+
+num = 9
+so = float(num)
+print(so)
+
+___list:- list()
+
+example:
+
+how= "12345"
+print(type(how))
+who = list(how)
+print(type(who))
+
+___tuple:- tuple()
+
+example:
+
+how= "12345"
+print(type(how))
+who = list(how)
+print(type(who))
+
+___list to tuple:
+
+example:
+
+how= [1,2,3,4]
+print(type(how))
+who = tuple(how)
+print(type(who))
+
+tuple to list:
+
+example:
+
+how= [1,2,3,4]
+print(type(how))
+who = list(how)
+print(type(who))
+
+
+
+'''
+how= [1,2,3,4]
+print(type(how))
+who = list(how)
+print(type(who))
